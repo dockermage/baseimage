78ff1b928534fc5def4e4fd85c61063f6824342c
Vlad Fratila <vlad.fratila@gmail.com>
2014-11-02 18:59:01 +0200

    remove python3, my_init works with python 2 too

--- image/prepare.sh
+++ image/prepare.sh
@@ -50,5 +50,3 @@ $minimal_apt_get_install locales
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 echo 'LANG="en_US.UTF-8"' > /etc/default/locale
 
-## python3
-$minimal_apt_get_install python3
