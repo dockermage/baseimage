f6cf89566a0ef5bae597c4d64cc5f47ab0417277
Vlad Fratila <vlad.fratila@gmail.com>
2014-11-05 00:02:02 +0200

    add environment var to remove ssh at boot

--- image/00_regen_ssh_host_keys.sh
+++ image/00_regen_ssh_host_keys.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
+source /etc/container_environment.sh
+
+if [ $DOCKER_DISABLE_SSH ]; then
+	echo "Found DOCKER_DISABLE_SSH, removing sshd"
+	rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
+	exit 0
+fi
+
 if [[ ! -e /etc/ssh/ssh_host_rsa_key ]]; then
 	echo "No SSH host key available. Generating one..."
 	export LC_ALL=C
